e0293571366fc77c193220c9a08870bf199ea039
mks <miksty>
2026-09-11 07:51:40 +0300

    repo add

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,15 +1,23 @@
+//config
 pub mod config {
     pub mod cfg;
 }
+use crate::config::cfg::Cfg;
 
 pub mod dto;
-pub mod handlers;
 pub mod models;
+
+pub mod handlers;
 pub mod router;
+
+pub mod repositories;
 pub mod services;
 
+//constants
 const CONFIG_DIR: &str = "config.toml";
 const DB: &str = "db";
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let config = Cfg::load(CONFIG_DIR);
+}
