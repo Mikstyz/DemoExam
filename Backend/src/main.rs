6017b2e2cd8ca2cdd9f5ff0fde4fa26204293e17
mks <miksty>
2026-09-11 04:00:26 +0300

    Initial commit: backend and frontend

---
+++ Backend/src/main.rs
@@ -0,0 +1,13 @@
+pub mod config {}
+
+pub mod dto;
+pub mod handlers;
+pub mod models;
+pub mod router;
+pub mod services;
+
+const CONFIG_DIR: &str = "config.toml";
+const DB: &str = "db";
+
+fn main() {}
+
