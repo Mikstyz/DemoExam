044b510b3c2335e9d8c880e0d96cd3f88c7665a6
miksty <ehouz85@gmail.com>
2026-09-17 02:54:50 +0300

    password to password hash

--- Backend/src/models/user.rs
+++ Backend/src/models/user.rs
@@ -8,7 +8,7 @@ pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
     pub login: String,
-    pub password: String,
+    pub password_hash: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
